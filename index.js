13fc6bb5525306a6c2dfe8035950fdad256feb3b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-03 19:36:31 +0100

    deploy monolith

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 4000
+const path = require('path')
 
 console.log('web 36 rocks')
 console.log(__dirname)
@@ -18,10 +19,15 @@ console.log(process.env.PORT)
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 // api, routers
 app.use('/api/*', (_, res) => {
   res.json({ data: 'web 36 rocks'})
+})
+
+app.use('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.listen(port, () => {

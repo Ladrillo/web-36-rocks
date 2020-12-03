ee895bdd3db91e01f7e10bc9af03d1d94545e9cb
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-03 19:33:56 +0100

    wip

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Web 36 rocks
+      </header>
+    </div>
+  );
+}
+
+export default App;

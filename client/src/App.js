67382e86e3b663bc9e985ed02fa3cb82f4ed67a5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-03 19:53:36 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+
+
+// development
+// production
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+    ? `http://localhost:5000${path}`
+    : ''
+}
 
 function App() {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    // https://heroku.com/nice-app
+    // http://localhost:5000
+    fetch(url('/api/data'))
+      .then(res => res.json())
+      .then(goods => setData(goods.data))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        Web 36 rocks
+        {data}
       </header>
     </div>
   );
